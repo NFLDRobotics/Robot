8170eadbd218481fce10d87a48a89fd4a98dca9f
halvqr <halvor.bratland@gmail.com>
2016-09-27 19:35:38 -0500

    importing folders
    except for build

---
+++ src/edu/wpi/first/wpilibj/templates/ClimberActuatorIn.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author Developer
+ */
+class ClimberActuatorIn {
+
+    static void set(boolean b) {
+    }
+    
+}
